fb328d6d77a624368ffaf9e4e8d28047920a43b8
Samuel Colin <scolin@hivernal.org>
2025-08-18 00:34:56 +0200

    Restart from Oryx sources

--- OWY7g/config.h
+++ OWY7g/config.h
@@ -1,8 +1,4 @@
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
-*/
-
+#define CHORDAL_HOLD
 #undef TAPPING_TERM
 #define TAPPING_TERM 180
 
@@ -12,7 +8,7 @@
 #define RGB_MATRIX_TIMEOUT 300000
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "OWY7g/qELML"
+#define SERIAL_NUMBER "OWY7g/AzwxNx"
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
